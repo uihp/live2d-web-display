eb09690e5c79ffd3d5365aeaa3e511a2efc870e2
uihp <1439379954@qq.com>
2022-12-05 22:26:54 +0800

    subdivided user's script to hide private models

--- index.js
+++ index.js
@@ -1,55 +1,13 @@
+import { modelConfigs } from './config.js'
+
+var displayer
+
 window.onload = () => {
   canvas.width = document.body.clientWidth
   canvas.height = document.body.clientHeight
-  displayer = new Live2DWebDisplay.Displayer(canvas, {
-    modelConfigs: [
-      {
-        dirPath: 'res/Haru/',
-        fileName: 'Haru.model3.json',
-        idle: 'Idle',
-        expression: 'Head',
-        interaction: {
-          'Body': 'TapBody'
-        }
-      },
-      {
-        dirPath: 'res/Hiyori/',
-        fileName: 'Hiyori.model3.json',
-        idle: 'Idle',
-        interaction: {
-          'Body': 'TapBody'
-        }
-      },
-      {
-        dirPath: 'res/Rice/',
-        fileName: 'Rice.model3.json',
-        idle: 'Idle',
-        interaction: {
-          'Body': 'TapBody'
-        }
-      },
-      {
-        dirPath: 'res/Mao/',
-        fileName: 'Mao.model3.json',
-        idle: 'Idle',
-        expression: 'Head',
-        interaction: {
-          'Body': 'TapBody'
-        }
-      },
-      {
-        dirPath: 'res/Mark/',
-        fileName: 'Mark.model3.json',
-        idle: 'Idle',
-        interaction: {
-          'Body': 'TapBody'
-        }
-      }
-    ]
-  })
+  displayer = new Live2DWebDisplay.Displayer(canvas, { modelConfigs })
   displayer.initialize()
   displayer.run()
-  this.displayer = displayer
 }
-window.onbeforeunload = () => this.displayer.release()
-button.onclick = () => this.displayer.manager.nextScene()
+window.onbeforeunload = () => displayer.release()
+button.onclick = () => displayer.manager.nextScene()
